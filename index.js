ccc685603cab2522074a4956792ee96e5a759385
rmahato64 <rmahato64@gmail.com>
2020-07-18 01:42:02 +0300

    server connected with mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://react-blog:rakesh1990@reactblog.bpclo.mongodb.net/<dbname>?retryWrites=true&w=majority',{useNewUrlParser:true }).then(()=>console.log('DB connected'))
+                                                    .catch(err=>console.error(err))
 
 app.get('/',(req,res)=>{
     res.send('Hello World')
